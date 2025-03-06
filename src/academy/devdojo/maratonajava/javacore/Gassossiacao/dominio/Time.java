4a605518c63fb48c9c2d4d8524fd4851e953297e
Gabriel <GabrielGonzaga042@gmail.com>
2025-03-06 16:52:03 -0300

    JogadorTest03

--- src/academy/devdojo/maratonajava/javacore/Gassossiacao/dominio/Time.java
+++ src/academy/devdojo/maratonajava/javacore/Gassossiacao/dominio/Time.java
@@ -2,6 +2,20 @@ package academy.devdojo.maratonajava.javacore.Gassossiacao.dominio;
 
 public class Time {
     private String nome;
+    private Jogador[] jogadores;
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
+    }
+
+    public Time(Jogador[] jogadores, String nome) {
+        this.jogadores = jogadores;
+        this.nome = nome;
+    }
 
     public Time(String nome) {
         this.nome = nome;
@@ -13,5 +27,16 @@ public class Time {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        if (jogadores == null) {
+            return;
+        }
+        for (Jogador jogador : jogadores) {
+            System.out.println(jogador.getNome());
+        }
+
     }
 }
