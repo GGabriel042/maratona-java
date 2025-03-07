f4a63a8e816663c6916a1023e9f55db129363a25
Gabriel <GabrielGonzaga042@gmail.com>
2025-03-07 16:00:26 -0300

    Enumeração 3

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -2,14 +2,17 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Gabriel", TipoCliente.PESSOA_FiSICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Gonzaga", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Gabriel", TipoCliente.PESSOA_FiSICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Gonzaga", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
