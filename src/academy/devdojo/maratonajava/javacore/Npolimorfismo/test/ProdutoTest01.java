c86d1f70f92069af1d80970179286ef92837d996
Gabriel <GabrielGonzaga042@gmail.com>
2025-03-07 20:33:39 -0300

    Polimorfism 2

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,16 +1,20 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
 public class ProdutoTest01 {
     public static void main(String[] args) {
-        Computador computador = new Computador("Windows", 5000);
-        Tomate tomate = new Tomate("Vermelho", 15);
+        Computador computador = new Computador("Windows i7", 5000);
+        Tomate tomate = new Tomate("Tomate vermelho", 15);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("-----------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("-----------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
