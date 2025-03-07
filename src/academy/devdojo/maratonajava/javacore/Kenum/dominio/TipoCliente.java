36759942f9218a9cd8dd386a80c999f5d8b44637
Gabriel <GabrielGonzaga042@gmail.com>
2025-03-07 16:10:08 -0300

    Enumeração 4

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,12 +1,29 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FiSICA(1),
-    PESSOA_JURIDICA(2);
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
 
     public final int VALOR;
+    private String nomeRelatorio;
 
-    TipoCliente(int valor) {
+    TipoCliente(int valor, String nomeRelatorio) {
         this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
     }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio) {
+        for (TipoCliente tipoCliente: values()) {
+            if(tipoCliente.getNomeRelatorio().equals(nomeRelatorio)) {
+                return tipoCliente;
+            }
+        }
+        return null;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
+
+    public int getVALOR() { return VALOR; }
 }
--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -6,7 +6,7 @@ import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Gabriel", TipoCliente.PESSOA_FiSICA, TipoPagamento.DEBITO);
+        Cliente cliente1 = new Cliente("Gabriel", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
         Cliente cliente2 = new Cliente("Gonzaga", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
 
@@ -14,5 +14,9 @@ public class ClienteTest01 {
         System.out.println(cliente2);
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica");
+        System.out.println(tipoCliente2);
     }
 }
