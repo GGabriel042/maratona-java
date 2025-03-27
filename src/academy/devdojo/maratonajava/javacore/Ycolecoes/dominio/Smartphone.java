a184670c929073ff64cf2576c02686b72b4f20b5
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-03-27 16:56:55 -0300

    Add files via upload

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
+
+public class Smartphone {
+    private String serialNumber;
+    private String marca;
+
+    public Smartphone(String serialNumber, String marca) {
+        this.serialNumber = serialNumber;
+        this.marca = marca;
+    }
+
+    //Reflexivo: x.equals(x) tem q ser true pra tudo diferente de null
+    //Simétrico: x.equals(y) == true ----- y.equals(x) tem q ser igual a true
+    //Transitividade:  x.equals(y) == true && x.equals(z) == true --------- y.equals(z) tem que ser igual a true
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
