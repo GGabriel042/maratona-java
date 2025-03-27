73719115da89e9eb28b177a6e036adaed39574eb
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-03-26 21:48:48 -0300

    Add files via upload

--- src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
@@ -1,11 +1,15 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.dominio;
 
-import java.io.Serializable;
+import java.io.*;
 
 public class Aluno implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 739959230178355185L;
     private Long id;
     private String nome;
-    private String password;
+    private transient String password;
+    private static final String NOME_ESCOLA = "DevDojo Viradão no Jiraya";
+    private transient Turma turma;
 
     public Aluno(Long id, String nome, String password) {
         System.out.println("Dentro do construtor");
@@ -14,12 +18,35 @@ public class Aluno implements Serializable {
         this.password = password;
     }
 
+    @Serial
+    private void writeObject(ObjectOutputStream oos){
+        try {
+            oos.defaultWriteObject();
+            oos.writeUTF(turma.getNome());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois){
+        try {
+            ois.defaultReadObject();
+            String nomeTurma = ois.readUTF();
+            turma = new Turma(nomeTurma);
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public String toString() {
         return "Aluno{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", password='" + password + '\'' +
+                ", NOME_ESCOLA='" + NOME_ESCOLA + '\'' +
+                ", Turma='" + turma + '\'' +
                 '}';
     }
 
@@ -45,5 +72,13 @@ public class Aluno implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
     }
 }
