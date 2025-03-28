e5c898bfb7cffad00133d7a7ef7be3b423e3f008
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-03-27 21:23:20 -0300

    Aula Coleções Sorting List comparable

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MangaSortTest01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "One piece", 19.99));
+        mangas.add(new Manga(1L, "Attack on titan", 19.99));
+        mangas.add(new Manga(4L, "Berserk", 25.99));
+        mangas.add(new Manga(3L, "Zatch bell", 30.99));
+        mangas.add(new Manga(2L, "Pokemon", 15.99));
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        System.out.println("-------------");
+        Collections.sort(mangas);
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+    }
+}
