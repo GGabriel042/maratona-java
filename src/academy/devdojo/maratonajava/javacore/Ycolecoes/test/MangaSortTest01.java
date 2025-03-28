2a0a5473ce7400b530093cab5d5dad2b465103d4
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-03-27 21:28:14 -0300

    Aula Coleções Sorting List comparator

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
@@ -4,15 +4,24 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class MangaByIdComparator implements Comparator<Manga> {
+
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
         List<Manga> mangas = new ArrayList<>(6);
         mangas.add(new Manga(5L, "One piece", 19.99));
-        mangas.add(new Manga(1L, "Attack on titan", 19.99));
+        mangas.add(new Manga(3L, "Attack on titan", 19.99));
         mangas.add(new Manga(4L, "Berserk", 25.99));
-        mangas.add(new Manga(3L, "Zatch bell", 30.99));
+        mangas.add(new Manga(1L, "Zatch bell", 30.99));
         mangas.add(new Manga(2L, "Pokemon", 15.99));
 
         for (Manga manga : mangas) {
@@ -20,8 +29,16 @@ public class MangaSortTest01 {
         }
 
         System.out.println("-------------");
-        Collections.sort(mangas);
 
+        Collections.sort(mangas);
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        System.out.println("-------------");
+
+//      Collections.sort(mangas, new MangaByIdComparator());
+        mangas.sort(new MangaByIdComparator());
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
