8fe03d7891c6c92b048ba8508774316497362c9e
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-04-03 20:51:02 -0300

    Add files via upload

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/FututeTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.*;
+
+public class FututeTest01 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollar = executorService.submit(new Callable<Double>() {
+            @Override
+            public Double call() throws Exception {
+                TimeUnit.SECONDS.sleep(2);
+                return 4.35D;
+            }
+        });
+        System.out.println(doingSomething());
+        Double dollarResponse = dollar.get(3, TimeUnit.SECONDS);
+        System.out.println("Dollar: "+dollarResponse);
+        executorService.shutdown();
+    }
+
+    private static long doingSomething() {
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
